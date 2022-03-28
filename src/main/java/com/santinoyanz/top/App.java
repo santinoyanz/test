096742b77330662f2a2aed46e1d9932ec6c39471
santinoyanz <santino.yanz@outlook.com>
2022-03-28 10:38:00 +0800

    Initial project version

---
+++ src/main/java/com/santinoyanz/top/App.java
@@ -0,0 +1,13 @@
+package com.santinoyanz.top;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        Utils.printMessage("Hello World!");
+    }
+}
---
+++ src/main/java/com/santinoyanz/top/Utils.java
@@ -0,0 +1,7 @@
+package com.santinoyanz.top;
+
+public class Utils {
+    public static void printMessage(String message) {
+            System.out.println(message);
+    }
+}
